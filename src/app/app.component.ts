e87511e48fe6e7f0fa0e41a4d8fb210a98a5012d
Ricard <>
2021-07-22 11:33:00 +0200

    splash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+//import { StatusBar } from '@ionic-native/status-bar/ngx';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,15 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen
+    //private statusBar: StatusBar
+  ) {}
+
+  initializeApp() {
+     this.platform.ready().then(() => {
+  //     this.statusBar.styleDefault();
+       this.splashScreen.hide();
+    })};
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  url_images:"https://pixabay.com/api/?key=22559493-70f3da81944305b36684c5c45&q=barcelona&image_type=photo",
 };
